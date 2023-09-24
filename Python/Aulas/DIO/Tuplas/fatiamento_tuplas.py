6543978a60c3651f338062dd9203e453f91eb021
Felipe Barros <sp44tksl@gmail.com>
2023-09-24 11:01:29 -0300

    Aula utilizando tuplas
    Exemplo de utilização de tuplas e suas diferença de uma lista e a utilização de seus metodos

---
+++ Python/Aulas/DIO/Tuplas/fatiamento_tuplas.py
@@ -0,0 +1,8 @@
+tupla = ("P", "y", "t", "h", "o", "n",)
+
+print(tupla[2:]) # ("t", "h", "o", "n")
+print(tupla[:2]) # ("P", "y")
+print(tupla[1:3]) # ("y", "t")
+print(tupla[0:3:2]) # ("P", "y", "t", "h", "o", "n")
+print(tupla[::]) # ("P", "y", "t", "h", "o", "n")
+print(tupla[::-1]) # ("n", "o", "h", "t", "y", "P")
---
+++ Python/Aulas/DIO/Tuplas/index.py
@@ -0,0 +1,4 @@
+linguagens = ("Python", "JavaScript", "C", "Java", "csharp",)
+
+print(linguagens.index("Java")) # 3
+print(linguagens.index("Python")) # 0
